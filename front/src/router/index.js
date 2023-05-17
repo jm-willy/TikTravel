f3bbc6a0afb284d3c9eefbb7e3e6dec3a0c09f95
Perandres8 <77155860+Perandres8@users.noreply.github.com>
2023-05-17 16:28:23 +0200

    estilos funcionan

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -21,7 +21,17 @@ const router = createRouter({
     {
       path: '/publicaciones',
       name: 'publicaciones',
-      component: () => import('../views/publicaciones.vue')
+      component: () => import('../views/Publicaciones.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/Login.vue')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/Register.vue')
     },
     // {
     //   path: '/reservas',
