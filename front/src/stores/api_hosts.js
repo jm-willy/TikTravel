6e44d4a025b79608649e140fd555608ff3105b9c
jm-willy <jaimewilly@hotmail.es>
2023-05-31 17:12:38 +0200

    u

--- front/src/stores/api_hosts.js
+++ front/src/stores/api_hosts.js
@@ -10,8 +10,8 @@ export const ApiHostStore = defineStore('debug status', {
         } else if (window.location.origin == state.dev_host) {
           return state.debug_host[0];
         } else {
-          // return state.production_host;
-          return '';
+          return state.production_host;
+          // return '';
         }
       },
       is_dev(state) {
